e6675c667c9f83fca9554d0c36fb79aa64bed32a
marcusharding <marcus.harding@kaldorgroup.com>
2021-12-04 14:12:32 +0000

    npm init

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+jQuery(function () {
+    total = Computer.total;
+    specs = Computer.specs;
+    $('.clickable').on('click', function (e) {
+        if ($(e.currentTarget).hasClass('buyButton')) {
+            // submit the form here
+            return;
+        }
+        var cat = $(this).parent().prev().text();
+        $(this).parent().find('.selected').removeClass('selected');
+        $(this).addClass('selected');
+        if (specs[cat].length != 0) {
+            total = total - specs[cat][1];
+        }
+        specs[cat] = [$(this).find('.component-option__name').text(), parseInt($(this).find('.component-option__price').text().substr(1), 10)];
+        total = total + parseInt($(this).find('.component-option__price').text().substr(1), 10);
+        $('.chosen-specs__spec-list').empty();
+        $('.chosen-specs__spec-list').append('<li class="spec-list__spec">' + specs.Screen[0] + '</li>');
+        $('.chosen-specs__spec-list').append('<li class="spec-list__spec">' + specs.CPU[0] + '</li>');
+        $('.chosen-specs__spec-list').append('<li class="spec-list__spec">' + specs.Storage[0] + '</li>');
+        $('.chosen-specs__spec-list').append('<li class="spec-list__spec">' + specs.Memory[0] + '</li>');
+        $('.chosen-specs__spec-list').append('<li class="spec-list__spec">' + specs.Graphics[0] + '</li>');
+        $('.summary-window__total').text('Total: £' + total);
+    });
+}) 
